3bf1152a16a5b4830816e957ba349a2641cfafda
randy-pucmm <20180622@ce.pucmm.edu.do>
2026-04-09 21:00:47 -0400

    generated evento module

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './db/prisma.module';
+import { EventoModule } from './evento/evento.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EventoModule],
   controllers: [AppController],
   providers: [AppService],
 })
